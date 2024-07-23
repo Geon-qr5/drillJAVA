0680ec557427e16fa8df1f90ae8a7804d22ab524
Geon-qr5 <zzzqr5@gmail.com>
2024-07-23 17:21:49 +0900

    필드연습

--- src/com/thisisjava/AcaApp.java
+++ src/com/thisisjava/AcaApp.java
@@ -4,8 +4,8 @@ public class AcaApp {
 public static void main(String[] args) {
     AcaExample aca = new AcaExample();
 
-    aca.lotto();
-    aca.numberSum();
-    aca.dice();
+    // aca.lotto2();
+    // aca.numberSum2();
+    aca.dice2();
 }
 }
