fcbfb616828806efacc73f086110174fc2bdc15c
home <zzzqr5@gmail.com>
2024-07-28 17:07:53 +0900

    집연습

---
+++ src/naver/java/ex01/Practice02.java
@@ -0,0 +1,16 @@
+package naver.java.ex01;
+
+public class Practice02 {
+    public static void main(String[] args) {
+        System.out.println(a());
+        System.out.println(one());
+    }
+
+    private static int one() {
+        return 1;
+    }
+
+    private static String a() {
+        return "a";
+    }
+}
